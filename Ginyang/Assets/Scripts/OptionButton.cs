0d4e20e1cedb1f3e04ba57aef232e97f4df04897
mithril0 <31401940+mithril0@users.noreply.github.com>
2018-12-08 21:55:00 +0900

    fdsa

---
+++ Ginyang/Assets/Scripts/OptionButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionButton : MonoBehaviour {
+    public GameObject Idle;
+    public GameObject Option;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OptionClicked()
+    {
+        Idle.SetActive(false);
+        Option.SetActive(true);
+    }
+    public void GobackGame()
+    {
+        Idle.SetActive(true);
+        Option.SetActive(false);
+    }
+}
