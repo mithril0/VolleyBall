79e765aa97b1fd826150dbb5f3de4a51fca8e4e0
Polariche <Polariche@gmail.com>
2018-12-08 22:55:58 +0900

    Create Invisible Pole script

---
+++ Ginyang/Assets/Scripts/InvisiblePole.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisiblePole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreCollision(GameObject.Find("Ball").GetComponent<Collider2D>(), GetComponent<Collider2D>());
+	}
+}
