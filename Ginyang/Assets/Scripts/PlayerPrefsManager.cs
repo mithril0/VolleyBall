8a37649e84fff7f95877e68957ff1f5d7f66a71f
mithril0 <31401940+mithril0@users.noreply.github.com>
2018-12-09 15:32:01 +0900

    사운드관리자

---
+++ Ginyang/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour {
+    public Slider volumeSlider;
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string HIGH_SCORE = "High Score";
+    void Start()
+    {
+        //musicManager = GameObject.FindObjectOfType<MusicManager>();
+
+        volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+    }
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= 0f && volume <= 1f)
+        {
+            PlayerPrefs.SetFloat("master_volume", volume);
+        }
+        else
+        {
+            Debug.LogError("Master Volume out of range");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void SetHighScore(int score)
+    {
+        PlayerPrefs.SetInt(HIGH_SCORE, score);
+    }
+    public void SaveAndLoadScene(string name)
+    {
+        PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+        SceneManager.LoadScene(name);
+    }
+    public static int GetHighScore()
+    {
+        return PlayerPrefs.GetInt(HIGH_SCORE);
+    }
+}
