050eab07ac76a7d7f2d80c69b616ca8cdb73cd77
mithril0 <31401940+mithril0@users.noreply.github.com>
2018-12-09 17:12:55 +0900

    점수창,위치초기화

--- Ginyang/Assets/Scripts/Ball.cs
+++ Ginyang/Assets/Scripts/Ball.cs
@@ -4,16 +4,75 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
     private AudioSource source;
+    public int colliding=0;
+    public float colldown = 0;
+    public int player = 0;
     // Use this for initialization
     private void Start()
     {
+
         source = GetComponent<AudioSource>();
     }
     void OnCollisionEnter2D (Collision2D c) {
-		Rigidbody2D rb = GetComponent<Rigidbody2D>();
-		rb.AddForce(c.contacts[0].normal * 400);
+        colliding++;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (c.collider.tag == "Player" && c.transform.position.y > -1)
+        {
+            /*if(c.transform.position.x > 0)
+            {
+                rb.AddForce(new Vector3(-1f,-0.5f,0) * 500);
+            }
+            else
+            {
+                rb.AddForce(new Vector3(1f, -0.5f, 0) * 500);
+            }*/
+            rb.AddForce(c.contacts[0].normal * 600);
+            //player++;
+        }
+        else
+        {
+            rb.AddForce(c.contacts[0].normal * 400);
+        }
         source.Play(0);
     
-        Debug.Log(rb.velocity);
+        Debug.Log(c.gameObject);
 	}
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        colliding--;
+        if (collision.collider.tag == "Player")
+        {
+           // player--;
+        }
+    }
+    private void Update()
+    {
+        colldown -= Time.deltaTime;
+        /*if (colliding > 1 && colldown < 0 && player > 0)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            if (gameObject.transform.position.x < 0)
+            {
+                if(gameObject.transform.position.x < 2)
+                {
+                    rb.AddForce(new Vector3(0.1f,0.7f,0) * 400);
+                }
+                else
+                {
+                    rb.AddForce(new Vector3(-0.5f, 0.7f, 0) * 400);
+                }
+            }
+            else {
+                if (gameObject.transform.position.x < -2)
+                {
+                    rb.AddForce(new Vector3(0.5f, 0.7f, 0) * 400);
+                }
+                else
+                {
+                    rb.AddForce(new Vector3(-0.1f, 0.7f, 0) * 400);
+                }
+            }
+            colldown = 0.5f;
+        }*/
+    }
 }
--- Ginyang/Assets/Scripts/Playscenemanager.cs
+++ Ginyang/Assets/Scripts/Playscenemanager.cs
@@ -6,30 +6,65 @@ using UnityEngine.UI;
 public class Playscenemanager : MonoBehaviour {
     public int score1;
     public int score2;
+    public float delay = 0.4f;
     public int maxscore;
     public Text t1;
     public Text t2;
+    public bool scored=false;
+    public Vector3 prepos;
+    public GameObject Ball;
+    public GameObject P1;
+    public GameObject P2;
+    public Rigidbody2D rb;
 
     // Use this for initialization
     void Start () {
+        
         score1 = 0;
         score2 = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (scored == true)
+        {
+            Time.timeScale = 0.2f;
+            delay -= Time.deltaTime;
+            if (delay < 0)
+            {
+                //Ball = GameObject.Find("Ball");
+                //Destroy(Ball);
+                rb.velocity = Vector2.zero;
+                //rb.angularVelocity = Vector3.zero;
+                scored = false;
+                delay = 0.4f;
+                //rb.velocity = new Vector3(0, 0, 0);
+                Ball.transform.position = prepos;
+                P1.transform.position = new Vector3(-2, -1.599f, -3);
+                P2.transform.position = new Vector3(2, -1.599f, -3);
+                Time.timeScale = 1;
+            }
+        }
         t1.text = score1.ToString();
         t2.text = score2.ToString();
 	}
     void OnCollisionEnter2D(Collision2D c)
     {
-        if (c.gameObject.transform.position.x < 0)
+
+        if (scored == false)
         {
-            score1++;
-        }
-        else
-        {
-            score2++;
+            if (c.gameObject.transform.position.x < 0)
+            {
+                score2++;
+                prepos = new Vector3(-2, 2, -0.5f);
+            }
+            else
+            {
+                score1++;
+                prepos = new Vector3(2, 2, -0.5f);
+
+            }
+            scored = true;
         }
     }
 }
