1628857d07841a9bf436111a6959b35ebd416a33
mithril0 <31401940+mithril0@users.noreply.github.com>
2018-12-10 18:14:13 +0900

    오프닝씬추가, 버튼클릭음추가, 승패더미 추가

---
+++ Ginyang/Assets/Scripts/OpeningScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OpeningScene : MonoBehaviour {
+    const string MASTER_VOLUME_KEY = "master_volume";
+    public float delay = 3;
+    // Use this for initialization
+    void Start () {
+        PlayerPrefs.SetFloat("master_volume", 0.5f);
+        Time.timeScale = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        delay -= Time.deltaTime;
+        if (delay < 0)
+        {
+            SceneManager.LoadScene("Start");
+        }
+	}
+}
